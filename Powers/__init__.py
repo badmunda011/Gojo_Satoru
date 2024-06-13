347a9cf24a8758521b206bd8ec0e07dfb72de966
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-06-14 00:23:14 +0530

    Update __init__.py

--- Powers/__init__.py
+++ Powers/__init__.py
@@ -99,7 +99,7 @@ elif not Config.GENIUS_API_TOKEN:
 is_audd = False
 Audd = None
 if Config.AuDD_API:
-    is_audd = True
+    is_audd = false
     Audd = Config.AuDD_API
     LOGGER.info("Found Audd api")
 
